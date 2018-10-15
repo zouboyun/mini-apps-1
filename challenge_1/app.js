1745cdac3b243fd9e8e6c379a816eb93cab15b15
Boyun Zou <zouboyun@Boyuns-MacBook-Pro.local>
2018-10-15 11:59:37 -0700

    added calculate winner helper function

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,6 +1,10 @@
-var squares = document.getElementsByClassName('square');
+/************************ select DOM nodes ************************/
+var squares = Array.from(document.getElementsByClassName('square'));
 var currentPlayer = document.getElementById('player');
 var resetBtn = document.getElementById('reset');
+
+
+/************************ define event handlers ************************/
 var clickHandler = event => {
     event.target.textContent = currentPlayer.textContent;
     if (currentPlayer.textContent === 'X') {
@@ -8,15 +12,41 @@ var clickHandler = event => {
     } else {
         currentPlayer.textContent = 'X';
     }
+    // check if there is a win or tie
+    if (calculateWinner() !== null) {
+        console.log('winner is ', calculateWinner());
+    }
 };
 var resetGame = event => {
     currentPlayer.textContent = 'X';
-    Array.from(squares).forEach(square => {
+    squares.forEach(square => {
         square.textContent = '';
     });
 };
 
-Array.from(squares).forEach(square => {
+/************************ define helper functions ************************/
+var calculateWinner = () => {
+    const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+      ];
+    for (let i = 0; i < lines.length; i++) {
+        const [a, b, c] = lines[i];
+        if (squares[a].textContent && squares[a].textContent === squares[b].textContent && squares[b].textContent === squares[c].textContent) {
+            return squares[a].textContent;
+        }
+    }
+    return null;
+};
+
+/************************ add event handlers to nodes ************************/
+squares.forEach(square => {
     square.addEventListener('click', clickHandler);
 });
 
