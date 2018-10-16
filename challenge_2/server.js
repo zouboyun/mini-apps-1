4923762a228502395a8b4a19c5c79ae40c903ec8
Boyun Zou <zouboyun@Boyuns-MacBook-Pro.local>
2018-10-16 13:25:14 -0700

    receiving form data from client to server

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,7 +1,22 @@
-var express = require('express')
+/*********************** require express ***********************/
+var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
 
+/*********************** configure body parser ***********************/
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+/*********************** serve up static assets ***********************/
 app.use(express.static(__dirname + '/client'));
+
+/*********************** configure routes ***********************/
+app.post('/upload_json', (req, res) => {
+    console.log(typeof req.body, req.body);
+    res.status(201).end();
+});
+
+/*********************** start server ***********************/
 app.listen(1337, () => {
     console.log('app is listening at port 1337');
 });
