d4944408881cf09d33a32931771daf29466624ac
Boyun Zou <zouboyun@Boyuns-MacBook-Pro.local>
2018-10-16 12:11:00 -0700

    configured server using express

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,7 @@
+var express = require('express')
+var app = express();
 
-
+app.use(express.static(__dirname + '/client'));
+app.listen(1337, () => {
+    console.log('app is listening at port 1337');
+});
