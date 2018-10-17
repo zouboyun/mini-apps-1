b74b590eaf090308e18f07c97b103dc5a61d877d
Boyun Zou <zouboyun@Boyuns-MacBook-Pro.local>
2018-10-17 11:29:19 -0700

    started server using express, moved index page from public to view

--- challenge_3/server.js
+++ challenge_3/server.js
@@ -1,2 +1,25 @@
+/*********************** require express ***********************/
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
 
 
+/*********************** configure app ***********************/
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.set('view engine', 'ejs');
+
+/*********************** serve up static assets ***********************/
+app.use(express.static(__dirname + '/public'));
+
+
+/*********************** configure routes ***********************/
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+
+/*********************** start server ***********************/
+app.listen(8090, () => {
+    console.log('app is listening at port 8090');
+});
