2d45838268d842d6b830c2141f65ce72febf21b9
Boyun Zou <zouboyun@Boyuns-MacBook-Pro.local>
2018-10-17 16:59:37 -0700

    updated db schema

--- challenge_3/db/schema.sql
+++ challenge_3/db/schema.sql
@@ -3,18 +3,34 @@ USE checkout;
 
 CREATE TABLE IF NOT EXISTS users (
   id              INT unsigned NOT NULL AUTO_INCREMENT,
-  name            VARCHAR(150) NOT NULL,
-  email           VARCHAR(150) NOT NULL,
+  name            VARCHAR(50) NOT NULL,
+  email           VARCHAR(50) NOT NULL,
+  address         VARCHAR(150) NOT NULL,
   createdAt       TIMESTAMP NOT NULL,
   updatedAt       DATETIME,
   PRIMARY KEY     (id)
 );
 
-CREATE TABLE IF NOT EXISTS orders (
+CREATE TABLE IF NOT EXISTS payments (
   id              INT unsigned NOT NULL AUTO_INCREMENT,
+  ccNumber        VARCHAR(50) NOT NULL,
+  cvv             VARCHAR(10) NOT NULL,
+  billingZip      VARCHAR(10) NOT NULL,
+  expirationDate  DATETIME,
   userId          INT unsigned NOT NULL,
   createdAt       TIMESTAMP NOT NULL,
   updatedAt       DATETIME,
   PRIMARY KEY     (id),
   FOREIGN KEY     (userId) REFERENCES users(id)
+);
+
+CREATE TABLE IF NOT EXISTS orders (
+  id              INT unsigned NOT NULL AUTO_INCREMENT,
+  userId          INT unsigned NOT NULL,
+  paymentId       INT unsigned NOT NULL,
+  createdAt       TIMESTAMP NOT NULL,
+  updatedAt       DATETIME,
+  PRIMARY KEY     (id),
+  FOREIGN KEY     (userId) REFERENCES users(id),
+  FOREIGN KEY     (paymentId) REFERENCES payments(id)
 );
